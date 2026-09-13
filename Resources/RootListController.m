eab0ea3a43745de07d1dc05ffbbf86d65eb72fda
trantu2641 <trantu2641@gmail.com>
2026-09-13 09:38:46 +0700

    Update RootListController.m

--- Resources/RootListController.m
+++ Resources/RootListController.m
@@ -1,17 +1,28 @@
-#import <Preferences/PSListController.h>
-
-@interface SCRootListController : PSListController
-@end
+#import "RootListController.h"
 
 @implementation SCRootListController
 
-- (NSArray *)specifiers {
-	if (!_specifiers) {
-		_specifiers = [self loadSpecifiersFromPlistName:@"Root"
-												 target:self];
-	}
+- (NSArray *)specifiers
+{
+    if (!_specifiers) {
+        _specifiers = [self loadSpecifiersFromPlistName:@"Root"
+                                                 target:self];
+    }
 
-	return _specifiers;
+    return _specifiers;
+}
+
+- (void)respring
+{
+    pid_t pid = [[NSProcessInfo processInfo] processIdentifier];
+
+    if (pid == 0) {
+        return;
+    }
+
+    // PreferenceLoader gọi method này từ PSButtonCell.
+    // Dùng SpringBoard restart thông qua launchctl.
+    system("killall -9 SpringBoard");
 }
 
 @end
