ebc562038d31c194edb7a53d67d3eac875ad4b5d
trantu2641 <trantu2641@gmail.com>
2026-09-13 16:17:46 +0700

    Update RootListController.m

--- Resources/RootListController.m
+++ Resources/RootListController.m
@@ -1,14 +1,23 @@
 #import "RootListController.h"
-#import <Preferences/Preferences.h>
+#import <Preferences/PSSpecifier.h>
 
 @implementation RootListController
 
 - (NSArray *)specifiers {
     if (!_specifiers) {
         _specifiers = [self loadSpecifiersFromPlistName:@"Root"
-                                                  target:self];
+                                                 target:self];
     }
+
     return _specifiers;
+}
+
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier {
+    [super setPreferenceValue:value specifier:specifier];
+
+    CFPreferencesAppSynchronize(
+        CFSTR("com.trantu2641.systemcorner")
+    );
 }
 
 @end
