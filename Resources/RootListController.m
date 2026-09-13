479ef864496f18aa5fa0e43a8c4e0374663d67d4
trantu2641 <trantu2641@gmail.com>
2026-09-13 19:54:38 +0700

    Update RootListController.m

--- Resources/RootListController.m
+++ Resources/RootListController.m
@@ -1,78 +1,28 @@
 #import "RootListController.h"
-#import <Preferences/PSSpecifier.h>
+#import <Preferences/Preferences.h>
+#import <UIKit/UIKit.h>
+#import <notify.h>
 
 @implementation RootListController
 
-- (NSArray *)specifiers {
+- (NSArray *)specifiers
+{
     if (!_specifiers) {
-        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+        _specifiers = [self loadSpecifiersFromPlistName:@"Root"
+                                                 target:self];
     }
 
     return _specifiers;
 }
 
-- (id)readPreferenceValue:(PSSpecifier *)specifier {
-    NSString *key = [specifier propertyForKey:@"key"];
-
-    if (!key) {
-        return nil;
-    }
-
-    id value = [[NSUserDefaults standardUserDefaults] objectForKey:key];
-
-    if (value == nil) {
-        value = [specifier propertyForKey:@"default"];
-    }
-
-    return value;
-}
-
-- (void)setPreferenceValue:(id)value
-              forSpecifier:(PSSpecifier *)specifier {
-
-    NSString *key = [specifier propertyForKey:@"key"];
-
-    if (!key) {
-        return;
-    }
-
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-
-    if (value) {
-        [defaults setObject:value forKey:key];
-    } else {
-        [defaults removeObjectForKey:key];
-    }
+- (void)applySettings
+{
+    NSUserDefaults *defaults =
+        [[NSUserDefaults alloc] initWithSuiteName:@"xyz.cypwn.systemcorner"];
 
     [defaults synchronize];
 
-    CFNotificationCenterPostNotification(
-        CFNotificationCenterGetDarwinNotifyCenter(),
-        CFSTR("xyz.cypwn.systemcorner.settingsChanged"),
-        NULL,
-        NULL,
-        YES
-    );
-}
-
-- (void)respring:(id)sender {
-    pid_t pid;
-
-    const char *args[] = {
-        "killall",
-        "-9",
-        "SpringBoard",
-        NULL
-    };
-
-    posix_spawn(
-        &pid,
-        "/usr/bin/killall",
-        NULL,
-        NULL,
-        (char *const *)args,
-        NULL
-    );
+    notify_post("xyz.cypwn.systemcorner.settingsChanged");
 }
 
 @end
